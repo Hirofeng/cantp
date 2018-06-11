94500c6ebf39f4afee3a652c5c56645410bf4e13
hirofeng_pc <408148917@qq.com>
2018-06-11 14:27:20 +0800

    增加unity 单元测试组件

---
+++ cantp/test/test_code_interface.h
@@ -0,0 +1,14 @@
+#ifndef _TEST_CODE_INTERFACE_H_
+#define _TEST_CODE_INTERFACE_H_
+
+#include "can_tp.h"
+
+
+extern U8 check_sf_dl(U8 can_dl, U8 sf_dl, BOOL ext_addr_flag);
+
+
+
+
+
+
+#endif
---
+++ cantp/test/test_runners/TestProductionCode_Runner.c
@@ -0,0 +1,8 @@
+#include "unity.h"
+#include "unity_fixture.h"
+
+TEST_GROUP_RUNNER(TpSingleFrame)
+{
+  RUN_TEST_CASE(TpSingleFrame, TestSFDataLength);
+
+}
